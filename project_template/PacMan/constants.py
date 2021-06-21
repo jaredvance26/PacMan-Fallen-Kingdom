dfd38213602b858d0d2e78c56abe1b8a81950d3b
Brandon Densmore <densmoreb@Brandons-MacBook-Pro.local>
2021-06-21 15:06:04 -0600

    added path os

--- project_template/PacMan/constants.py
+++ project_template/PacMan/constants.py
@@ -6,3 +6,7 @@ SCREEN_TITLE = 'Pac-Man'
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 PACCMAN_IMAGE = os.path.join(PATH, '..', 'pac-assets', 'character-pac-man.png')
+BLINKY_IMAGE = os.path.join(PATH, '..', 'pac-assets', 'character-blinky.png')
+CLYDE_IMAGE = os.path.join(PATH, '..', 'pac-assets', 'character-clyde.png')
+INKY_IMAGE = os.path.join(PATH, '..', 'pac-assets', 'character-inky.png')
+PINKY_IMAGE = os.path.join(PATH, '..', 'pac-assets', 'character-pinky.png')
