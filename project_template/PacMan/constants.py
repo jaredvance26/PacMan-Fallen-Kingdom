3bc89dea8992e481b1cdb41a4468cf045d75e228
Brandon Densmore <densmoreb@Brandons-MacBook-Pro.local>
2021-06-21 14:53:27 -0600

    added pacman

--- project_template/PacMan/constants.py
+++ project_template/PacMan/constants.py
@@ -4,3 +4,5 @@ MAX_X = 800
 MAX_Y = 600
 SCREEN_TITLE = 'Pac-Man'
 
+PATH = os.path.dirname(os.path.abspath(__file__))
+PACCMAN_IMAGE = os.path.join(PATH, '..', 'pac-assets', 'character-pac-man.png')
